e5eb3004380c0c9d4ebcbae6e4953c395dd3cbe4
gab-menin <gabrielm.boff@gmail.com>
2022-08-16 18:09:10 -0300

    Lista 1 - Exercício 1

---
+++ lista1/exercicio1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    float base, altura, area;
+
+    printf("Qual a base do triângulo?");
+    scanf("%f", &base);
+
+    printf("Qual a altura do triângulo?");
+    scanf("%f", &altura);
+
+    area = ( base * altura ) / 2;
+
+    printf("A área do triângulo é igual a:  %f\n", area);
+
+    return 0;
+}
