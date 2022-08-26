c112b978bb8ec7c25f43616972fe14b63834c9a9
gab-menin <gabrielm.boff@gmail.com>
2022-08-25 21:59:03 -0300

    Lista 2 - Exercício 4

---
+++ lista2/exercicio4/index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+4. Escreva um algoritmo para ler o nome e a idade de uma pessoa, e exibir quantos dias de
+vida ela possui. Considere sempre anos completos, e que um ano possui 365 dias. Ex: uma
+pessoa com 19 anos possui 6935 dias de vida; veja um exemplo de saída: MARIA, VOCÊ JÁ
+VIVEU 6935 DIAS 5.
+*/
+
+int main()
+{
+    char nome[256];
+    int
+        idade,
+        diasDeVida;
+
+    printf("Qual o seu nome? ");
+    scanf("%s", &nome);
+
+    printf("Qual a sua idade? ");
+    scanf("%d", &idade);
+
+    diasDeVida = idade * 365;
+
+    printf("%s, você já viveu %d dias.\n", nome, diasDeVida);
+
+    return 0;
+}
