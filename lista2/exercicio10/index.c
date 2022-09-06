9c92ca6e057116de71a35becc11e056c996ea0b2
gab-menin <gabrielm.boff@gmail.com>
2022-09-06 17:36:25 -0300

    Lista 2 - Exercício 10

--- lista2/exercicio10/index.c
+++ lista2/exercicio10/index.c
@@ -2,12 +2,34 @@
 #include <math.h>
 
 /*
-6. O restaurante a quilo Bem-Bão cobra R$12,00 por cada quilo de refeição. Escreva um
-algoritmo que leia o peso do prato montado pelo cliente (em quilos) e imprima o valor a pagar.
-Assume que a balança já desconte o peso do prato.
+10. Construa um algoritmo para calcular a distância entre dois pontos do plano cartesiano.
+Cada ponto é um par ordenado (x,y).
 */
 
 int main()
 {
+    float
+        x1,
+        y1,
+        x2,
+        y2,
+        distanciaEntreDoisPontos;
 
+    printf("Qual o valor de x1? ");
+    scanf("%f", &x1);
+
+    printf("Qual o valor de y1? ");
+    scanf("%f", &y1);
+
+    printf("Qual o valor de x2? ");
+    scanf("%f", &x2);
+
+    printf("Qual o valor de y2? ");
+    scanf("%f", &y2);
+
+    distanciaEntreDoisPontos = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+    printf("A distância entre dois é igual a %f.\n", distanciaEntreDoisPontos);
+
+    return 0;
 }
